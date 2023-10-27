397a63f8f013ee06a2c0803c23b4e5205a9fba2a
Ðvadimsteshkin <VASteshkin@mai.education>
2023-10-28 00:05:45 +0300

    вмер

--- second year/Programming/Lab2/main.cpp
+++ second year/Programming/Lab2/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <ctime>
-#include <cstdlib> // Для функции rand()
+#include <cstdlib>
 #include <vector>
-#include <algorithm> // Для сортировки массива
+#include <algorithm>
+
 using namespace std;
 
-// Функция для алгоритма BLS (Бинарный поиск)
 int binarySearch(const vector<int>& arr, int key, int& comparisons) {
     int left = 0;
     int right = arr.size() - 1;
     
     while (left <= right) {
         int mid = left + (right - left) / 2;
-        comparisons++; // Увеличиваем счетчик сравнений
+        comparisons++;
         if (arr[mid] == key) {
             return mid;
         } else if (arr[mid] < key) {
@@ -22,47 +22,71 @@ int binarySearch(const vector<int>& arr, int key, int& comparisons) {
         }
     }
     
-    return -1; // Элемент не найден
+    return -1;
 }
 
-// Функция для алгоритма SLS (Линейный поиск)
 int linearSearch(const vector<int>& arr, int key, int& comparisons) {
     for (int i = 0; i < arr.size(); i++) {
-        comparisons++; // Увеличиваем счетчик сравнений
+        comparisons++;
         if (arr[i] == key) {
             return i;
         }
     }
     
-    return -1; // Элемент не найден
+    return -1;
 }
 
-int main() 
-{
-    // Генерация данных
-    srand(time(0)); // Инициализация генератора случайных чисел
+int ordinaryArraySearch(const vector<int>& arr, int key, int& comparisons) {
+    for (int i = 0; i < arr.size(); i++) {
+        comparisons++;
+        if (arr[i] == key) {
+            return i;
+        } else if (arr[i] > key) {
+            break; // Так как массив отсортирован по неубыванию, можно прекратить поиск
+        }
+    }
+    
+    return -1;
+}
+
+int binarySearchSorted(const vector<int>& arr, int key, int& comparisons) {
+    int left = 0;
+    int right = arr.size() - 1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        comparisons++;
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+int main() {
+    srand(time(0));
     vector<int> sortedArray;
     vector<int> unsortedArray;
 
-    // Заполнение массивов случайными числами
-    for (int i = 0; i < 200000; i++) 
-    {
+    for (int i = 0; i < 200000; i++) {
         int num = rand();
         sortedArray.push_back(num);
         unsortedArray.push_back(num);
     }
 
-    // Сортировка sortedArray
     sort(sortedArray.begin(), sortedArray.end());
 
-    // Определите размерности массивов и ключ поиска
     int sizes[] = {10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};
-    int searchKey = sortedArray[0]; // Используйте ключ, который находится в начале массива
+    int searchKey = sortedArray[0];
 
-    // Проведение измерений
     for (int i = 0; i < 10; i++) {
         int size = sizes[i];
-        vector<int> subArray(sortedArray.begin(), sortedArray.begin() + size); // Выбираем подмассив
+        vector<int> subArray(sortedArray.begin(), sortedArray.begin() + size);
 
         int comparisonsBLS = 0;
         clock_t startBLS = clock();
@@ -76,9 +100,23 @@ int main()
         clock_t endSLS = clock();
         double durationSLS = double(endSLS - startSLS) / CLOCKS_PER_SEC;
 
+        int comparisonsOAS = 0;
+        clock_t startOAS = clock();
+        int resultOAS = ordinaryArraySearch(subArray, searchKey, comparisonsOAS);
+        clock_t endOAS = clock();
+        double durationOAS = double(endOAS - startOAS) / CLOCKS_PER_SEC;
+
+        int comparisonsBS = 0;
+        clock_t startBS = clock();
+        int resultBS = binarySearchSorted(subArray, searchKey, comparisonsBS);
+        clock_t endBS = clock();
+        double durationBS = double(endBS - startBS) / CLOCKS_PER_SEC;
+
         cout << "Size: " << size << endl;
         cout << "BLS: Time - " << durationBLS << "s, Comparisons - " << comparisonsBLS << endl;
         cout << "SLS: Time - " << durationSLS << "s, Comparisons - " << comparisonsSLS << endl;
+        cout << "OAS: Time - " << durationOAS << "s, Comparisons - " << comparisonsOAS << endl;
+        cout << "BS: Time - " << durationBS << "s, Comparisons - " << comparisonsBS << endl;
         cout << "-----------------------------------------" << endl;
     }
 
---
+++ second year/Programming/Lab4/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+
+void shellSort(std::vector<int>& arr) {
+    int n = arr.size();
+    int gap = n / 2;
+
+    while (gap > 0) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j = i;
+
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+
+            arr[j] = temp;
+        }
+        gap /= 2;
+    }
+}
+
+void cocktailSort(std::vector<int>& arr) {
+    int n = arr.size();
+    bool swapped = true;
+    int start = 0;
+    int end = n - 1;
+
+    while (swapped) {
+        swapped = false;
+
+        for (int i = start; i < end; i++) {
+            if (arr[i] > arr[i + 1]) {
+                std::swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+
+        swapped = false;
+        end--;
+
+        for (int i = end; i > start; i--) {
+            if (arr[i] < arr[i - 1]) {
+                std::swap(arr[i], arr[i - 1]);
+                swapped = true;
+            }
+        }
+        start++;
+    }
+
+}
+}
+
+int main() 
+{
+    std::vector<int> data = { };
+    for (int size :  {
+        std::vector<int> data(size);
+        int key = data[size / 2]; // Ключ ищется в центре
+
+        for (int i = 0; i < size; ++i) {
+            data[i] = rand() % size;
+        }
+    cocktailSort(random_array);
+    shellSort(random_array);
+    // Выведите массив на каждом промежуточном этапе сортировки и сравните счетчики сравнений и пересылок
+    }
